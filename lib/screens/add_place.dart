f2774fa72a3debb268891b87295dae6c7b4f131d
luis <luisfernando.anemona@gmail.com>
2024-05-24 12:23:54 -0600

    implementación de google maps api

--- lib/screens/add_place.dart
+++ lib/screens/add_place.dart
@@ -22,12 +22,14 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
   PlaceLocation? _selectedLocation;
   void _savePlace() {
     final enteredTitle = _titleController.text;
-    if (enteredTitle.isEmpty || _selectedImage == null||_selectedLocation==null) {
+    if (enteredTitle.isEmpty ||
+        _selectedImage == null ||
+        _selectedLocation == null) {
       return;
     }
     ref
         .read(userPlacesProvider.notifier)
-        .addPlace(enteredTitle, _selectedImage!,_selectedLocation!);
+        .addPlace(enteredTitle, _selectedImage!, _selectedLocation!);
     Navigator.of(context).pop();
   }
 
